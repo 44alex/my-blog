2f3fd66367f70dc123d81a02d0008873a135f828
System Administrator <root@Ices-MBP.verizon.net>
2020-05-05 23:53:49 -0400

    First commit

---
+++ src/build/precache-manifest.da2fdf84c967d34024aa4203f677d2cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60efeb2a213f6ca6e8cd6992610f0f39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6cc123a011d1e8ff8e6",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "6ac32d2e9b58823ea516",
+    "url": "/static/js/2.aa04e143.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aa04e143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6cc123a011d1e8ff8e6",
+    "url": "/static/js/main.51aaa2ac.chunk.js"
+  },
+  {
+    "revision": "053c8e34507918769709",
+    "url": "/static/js/runtime-main.aecaeffa.js"
+  }
+]);
